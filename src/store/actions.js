628359e4a503680fbabba05796a1e563c30bfa88
吳任貴 <87403995+coollkk@users.noreply.github.com>
2021-08-18 18:29:10 +0800

    Delete src directory

--- src/store/actions.js
+++
@@ -1,9 +0,0 @@
-import {
-  RECEIVE_TODOS
-} from './mutationtypes'
-
-export default {
-  handleSubmit ({commit}, {todos, others}) {
-    commit(RECEIVE_TODOS, {todos, others})
-  }
-}
--- src/store/mutations.js
+++
@@ -1,9 +0,0 @@
-import {
-  RECEIVE_TODOS
-} from './mutationtypes'
-export default {
-  [RECEIVE_TODOS] (state, {todos, others}) {
-    state.todos = todos
-    state.others = others
-  }
-}
